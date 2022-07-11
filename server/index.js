97c2f4fb204a9550948bbf1c5dfabe4a913ae151
Tunisia Artope <artope@yahoo.com>
2022-07-11 15:53:54 -0400

    create get all, get one, update, and delete routes

--- server/index.js
+++ server/index.js
@@ -18,21 +18,57 @@ app.use(express.json());  //req.body object: gets data from the client. gives ac
 app.post('/todos', async(req,res)=>{
     //try/catch is for error handling
     try {
-        const {description} = req.body;
+        const { description } = req.body;
         const newTodo = await pool.query('INSERT INTO todos (description) VALUES($1) RETURNING *', [description]);  //'[description]' inside the query is the value of $1 (allows dynamic data). 'RETURNING *' used to return the data.
         res.json(newTodo.rows[0]);
     } catch (error) {
         console.error(err.message);
     }
-})
+});
 
 //get all todos 
+app.get('/todos', async(req,res)=>{
+    try {
+        const allTodos = await pool.query('SELECT * FROM todos');
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 //get a todo
+app.get('/todos/:id', async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const todo = await pool.query('SELECT * FROM todos WHERE todo_id = $1', [id]);  //'WHERE' specifies the type of todo wanted
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 //update a todo
+app.put('/todos/:id/', async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query('UPDATE todos SET description = $1 WHERE todo_id = $2', [description, id]);
+        res.json('Todo was update!');
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 //delete a todo
+app.delete('/todos/:id/', async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query('DELETE FROM todos WHERE todo_id = $1', [id]);
+        res.json('Todo was deleted!');
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 //gets the server to start
 //'nodemon index' command detects changes in the file and restarts server
